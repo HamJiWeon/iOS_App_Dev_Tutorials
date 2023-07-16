e2ace2e580c116dc5866d0aae2d93f59d82ecf5f
HamJi <skxo0039@gmail.com>
2023-07-16 14:35:03 +0900

    Initial Commit

---
+++ Scrumdinger/Views/MeetingFooterView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct MeetingFooterView: View {
+    let speakers: [ScrumTimer.Speaker]
+    var skipAction: ()->Void
+    
+    private var speakerNumber: Int? {
+        guard let index = speakers.firstIndex(where: { !$0.isCompleted }) else { return nil }
+        return index + 1
+    }
+    private var isLastSpeaker: Bool {
+        return speakers.dropLast().allSatisfy { $0.isCompleted }
+    }
+    private var speakerTest: String {
+        guard let speakerNumber = speakerNumber else { return "No more speaker" }
+        return "Speaker \(speakerNumber) od \(speakers.count)"
+    }
+    var body: some View {
+        VStack {
+            HStack {
+                if isLastSpeaker {
+                    Text("Last Speaker")
+                } else {
+                    
+                    Text(speakerTest)
+                    Spacer()
+                    Button(action: skipAction) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next Speaker")
+                }
+            }
+        }
+        .padding([.bottom, .horizontal])
+    }
+}
+
+struct MeetingFooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers, skipAction: {})
+            .previewLayout(.sizeThatFits)
+    }
+}
